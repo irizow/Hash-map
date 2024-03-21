62f6251c32c25695c3b84455e1c5db135601896b
irizow <irisrossell@gmail.com>
2024-03-22 00:25:35 +0800

    Make hash function

---
+++ Hashmap.js
@@ -0,0 +1,16 @@
+function hash(key) {
+    let hashCode = 0;
+       
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+ 
+    return hashCode;
+  } 
+
+class HashMap {
+    constructor() {
+        
+    }
+}
